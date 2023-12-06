5e41368ce58ee562ded5791844e22ae4f1604e57
Fira Hanasti <57513399+fhana123@users.noreply.github.com>
2023-12-06 17:26:02 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,212 @@
-// Get the modal
-var modal = document.getElementById('id01');
+const modal = document.getElementById('id01');
 
-// kalo click bebas bakal close
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function displayModal() {
+  if (modal) {
+    modal.style.display = 'block';
+  }
 }
+
+if (modal) {
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
+
+
+function logout() {
+  try {
+    localStorage.removeItem('loggedInUser');
+    fetch('http://localhost:3000/api/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.status === 'success') {
+        alert('Logout successful!');
+        window.location.href = 'login.html';
+      } else {
+        alert('Logout failed.');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Logout failed. Please try again later.');
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred during logout.');
+  }
+}
+
+document.querySelector('.logout').addEventListener('click', function(event) {
+  event.preventDefault();
+  logout();
+});
+
+// Pada fungsi checkLoginStatus()
+function checkLoginStatus() {
+  const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+  const logoutButton = document.querySelector('.logout');
+  if (loggedInUser && logoutButton) {
+    logoutButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      logout();
+    });
+    showLoggedInContent();
+  } else {
+    showLoggedOutContent();
+  }
+}
+
+// Pada fungsi showLoggedInContent() dan showLoggedOutContent()
+function showLoggedInContent() {
+  const logoutButton = document.querySelector('.logout');
+  const loginButton = document.querySelector('.login'); // Tambahkan variabel untuk tombol login jika perlu
+  if (logoutButton) {
+    logoutButton.style.display = 'block';
+  }
+  if (loginButton) {
+    loginButton.style.display = 'none'; // Sembunyikan tombol login jika perlu
+  }
+}
+
+function showLoggedOutContent() {
+  const logoutButton = document.querySelector('.logout');
+  const loginButton = document.querySelector('.login'); // Tambahkan variabel untuk tombol login jika perlu
+  if (logoutButton) {
+    logoutButton.style.display = 'none'; // Sembunyikan tombol logout jika perlu
+  }
+  if (loginButton) {
+    loginButton.style.display = 'block';
+  }
+}
+
+
+document.querySelector('.signupbtn').addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const fullname = document.querySelector('input[name="fullname"]').value;
+  const username = document.querySelector('input[name="username"]').value;
+  const email = document.querySelector('input[name="signupemail"]').value;
+  const password = document.querySelector('input[name="signuppassword"]').value;
+
+  if (!fullname || !username || !email || !password) {
+    alert('Semua kolom harus diisi.');
+    return;
+  }
+
+  const data = {
+    fullname: fullname,
+    username: username,
+    email: email,
+    password: password
+  };
+
+  fetch('http://localhost:3000/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (data.status === 'success') {
+      alert('Signup successful!');
+      const userInfo = document.createElement('div');
+      userInfo.innerHTML = `
+        <p>Status: ${data.status}</p>
+        <p>Message: ${data.message}</p>
+        <p>User:</p>
+        <ul>
+          <li>Full Name: ${data.user.fullname}</li>
+          <li>Username: ${data.user.username}</li>
+          <li>Email: ${data.user.email}</li>
+        </ul>
+      `;
+      document.body.appendChild(userInfo);
+      localStorage.setItem('loggedInUser', JSON.stringify(data.user));
+      window.location.href = 'index.html';
+    } else {
+      alert('Signup failed.');
+    }
+    console.log('Failed signup attempt with email:', data.email);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('Signup failed. Please try again later.');
+  });
+});
+
+document.querySelector('.btn').addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('input[name="loginemail"]').value;
+  const password = document.querySelector('input[name="loginpassword"]').value;
+
+  if (!email || !password) {
+    alert('Email dan password harus diisi.');
+    return;
+  }
+  const data = {
+    email: email,
+    password: password
+  };  
+  
+  fetch('http://localhost:3000/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (data.status === 'success') {
+      alert('Login successful!');
+      localStorage.setItem('loggedInUser', JSON.stringify(data.user));
+      showLoggedInContent();
+      window.location.href = 'index.html'; 
+
+      const userInfo = document.createElement('div');
+      userInfo.innerHTML = `
+        <p>Status: ${data.status}</p>
+        <p>Message: ${data.message}</p>
+        <p>User:</p>
+        <ul>
+          <li>Full Name: ${data.user.fullname}</li>
+          <li>Username: ${data.user.username}</li>
+          <li>Email: ${data.user.email}</li>
+        </ul>
+      `;
+      document.body.appendChild(userInfo);
+    } else {
+      alert('Login failed. Invalid email or password.');
+    }
+    console.log('Failed login attempt with email:', data.email);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('Login failed. Please try again later.');
+  });
+});
