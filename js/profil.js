038b28abbf46b1a0a84d379fdfc07a529fc03197
Fira Hanasti <57513399+fhana123@users.noreply.github.com>
2023-12-07 10:36:40 +0700

    Update profil.js

--- js/profil.js
+++ js/profil.js
@@ -8,6 +8,35 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('fullName').textContent = loggedInUser.fullname;
         document.getElementById('username').textContent = loggedInUser.username;
         document.getElementById('email').textContent = loggedInUser.email;
+
+        // Tambahkan event listener untuk tombol logout
+        const logoutButton = document.querySelector('.logout');
+        logoutButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Hindari pengiriman default dari link
+
+            // Lakukan permintaan logout ke server
+            fetch('http://localhost:3000/api/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Logout failed');
+            })
+            .then(data => {
+                alert(data.message); // Tampilkan pesan alert dari server
+                localStorage.removeItem('loggedInUser'); // Hapus data dari localStorage
+                window.location.href = 'login.html'; // Redirect ke halaman login setelah logout berhasil
+            })
+            .catch(error => {
+                console.error('Logout error:', error);
+                alert('Logout failed');
+            });
+        });
     } else {
         // Redirect ke halaman login jika tidak ada pengguna yang login
         window.location.href = 'login.html';
