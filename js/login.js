fdd476e38a5effdfe464d2859329956789b06a2a
Rizky017 <148648238+Rizky017@users.noreply.github.com>
2023-12-08 13:19:59 +0700

    perbaikan

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const DATABASE_URL = process.env.DATABASE_URL || "postgresql://postgres:bD4-ba2BA4EF1CG1e25adBdE3D2gCg*E@roundhouse.proxy.rlwy.net:34839/railway";
+const DATABASE_URL = "postgresql://postgres:bD4-ba2BA4EF1CG1e25adBdE3D2gCg*E@roundhouse.proxy.rlwy.net:34839/railway";
 
 const modal = document.getElementById('id01');
 
