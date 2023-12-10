a75a5408bc7f27da91b647768661b071b06cb57a
Rizky017 <148648238+Rizky017@users.noreply.github.com>
2023-12-10 17:55:14 +0700

    perubahan

--- js/login.js
+++ js/login.js
@@ -20,7 +20,7 @@ if (modal) {
 function logout() {
   try {
     localStorage.removeItem('loggedInUser');
-    fetch('http://localhost:3000/logout', { // Ubah endpoint menjadi '/logout'
+    fetch(`${API_BASE_URL}/logout`, { // Ubah endpoint menjadi '/logout'
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -106,7 +106,7 @@ function signup() {
     password: password
   };
 
-  fetch('http://localhost:3000/signup', {
+  fetch(`${API_BASE_URL}/signup`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -160,7 +160,7 @@ function login() {
     password: password
   };  
   
-  fetch('http://localhost:3000/login', {
+  fetch(`${API_BASE_URL}/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
